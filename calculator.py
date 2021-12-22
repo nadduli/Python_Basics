e319b0d0259c526f2af4d824cf97fc209aaacd63
nadduli <naddulidaniel94@gmail.com>
2021-12-23 01:03:07 +0300

    interest

--- calculator.py
+++ calculator.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """ basic calculator for +_*/
 """
-num1, operator, num2 = input().split()
+num1, operator, num2 = input('Enter Calculation: ').split()
 num1 = int(num1)
 num2 = int(num2)
 
 if operator == '+':
     print("{} + {} = {}".format(num1, num2, num1+num2))
 elif operator == '-':
-    print("{} + {} = {}".format(num1, num2, num1-num2))
+    print("{} - {} = {}".format(num1, num2, num1-num2))
 elif operator == '*':
-    print("{} + {} = {}".format(num1, num2, num1*num2))
+    print("{} * {} = {}".format(num1, num2, num1*num2))
 elif operator == '/':
-    print("{} + {} = {}".format(num1, num2, num1/num2))
+    print("{} / {} = {}".format(num1, num2, num1/num2))
 else:
     print("Invalid operand!")
