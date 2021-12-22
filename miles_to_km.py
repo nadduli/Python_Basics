3f06564716e20a12cf37d6e7209958cc47b68020
nadduli <naddulidaniel94@gmail.com>
2021-12-22 14:22:59 +0300

    miles to kilometer convertor

---
+++ miles_to_km.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+convert miles to kilometers
+"""
+miles = input('Enter miles: ')
+miles = int(miles)
+kilometers = miles * 1.60934
+print('{} miles equals to {}'.format(miles, kilometers))
